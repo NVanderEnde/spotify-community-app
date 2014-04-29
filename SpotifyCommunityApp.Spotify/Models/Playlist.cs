ba60954b539a10d2aa30ed17a63b5d2d975b6dd6
unknown <nvanderende@gmail.com>
2014-04-28 20:06:57 -0700

    Added basic Spotify API models

---
+++ SpotifyCommunityApp.Spotify/Models/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyCommunityApp.Spotify.Models
+{
+    class Playlist
+    {
+        string Name { get; set; }
+        string ImageURI { get; set; }
+        User Owner { get; set; }
+        IEnumerable<Track> Tracks { get; set; }
+    }
+}
